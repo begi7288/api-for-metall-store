6718bbeac9f997cf320ef30f2cd5f99c374e25d2
begi7288 <begi7288@gmail.com>
2026-07-22 11:00:33 +0500

    Add Swagger UI routes (/swagger/, /docs/, /swagger-ui/)

--- temirdokon_v1/urls.py
+++ temirdokon_v1/urls.py
@@ -65,6 +65,9 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-alias'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-direct'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
